4e397d668ad4e333dc1d98c4736696054e4ceab1
JosephGabrielCheng <Josephmeow2009@gmail.com>
2025-01-22 01:40:21 +0000

    submit

--- python_practice/next_year.py
+++ python_practice/next_year.py
@@ -1,6 +1,11 @@
+#Asking your name.
 print("Hello, World!")
 name = input("Enter your name:")
+#Greeting you.
 print(f"Hello,{name}")
+#Asking your age
 age = int(input("Enter your age"))
+#Greeting you and saying your age this year.
 print(f"Hello {name}! You are {age} years old")
+#Saying your age you are turning next year.
 print(f"Next year you will be {age+1} years old.")
